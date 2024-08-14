6d4fcabdd36d6d4bd6c1f74c09191da343dc5458
Randy <dddrandy@gmail.com>
2024-08-14 23:45:47 +0930

    Feat: Update project structure and add JetBrains compose.

---
+++ composeApp/src/iosMain/kotlin/org/cdu/codefair/alertcity/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.cdu.codefair.alertcity
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
--- shared/src/commonMain/kotlin/org/cdugroup/alertcity/Platform.kt
+++
@@ -1,7 +0,0 @@
-package org.cdugroup.alertcity
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
