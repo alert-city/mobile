b4d6c9c3fbddde1da3ef9d426b07c8d17cca2e8a
Randy <dddrandy@gmail.com>
2024-08-15 22:17:33 +0930

    Feat: Add login page UI.

--- composeApp/src/commonMain/kotlin/org/cdu/codefair/alertcity/App.kt
+++ composeApp/src/commonMain/kotlin/org/cdu/codefair/alertcity/App.kt
@@ -1,37 +1,29 @@
 package org.cdu.codefair.alertcity
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import org.cdugroup.alertcity.view.LoginPage
+import org.cdugroup.alertcity.view.MainScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import alertcity.composeapp.generated.resources.Res
-import alertcity.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+        var isLoggedIn by remember { mutableStateOf(false) }
+        var loggedInUsername by remember { mutableStateOf("") }
+
+        if (isLoggedIn) {
+            MainScreen(username = loggedInUsername)
+        } else {
+            LoginPage(onLoginSuccess = { username ->
+                isLoggedIn = true
+                loggedInUsername = username
+            })
         }
     }
 }
--- composeApp/src/commonMain/kotlin/org/cdu/codefair/alertcity/Platform.kt
+++
@@ -1,7 +0,0 @@
-package org.cdu.codefair.alertcity
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
