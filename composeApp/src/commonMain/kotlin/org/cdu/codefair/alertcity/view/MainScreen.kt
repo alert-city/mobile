7c986c107cefa2e9fe8810d22758ac2815a9f75a
Randy <dddrandy@gmail.com>
2024-08-24 23:48:24 +0930

    Feat: Add apollo graphql libraries and graphql schema.
    Finalised following features:
    - implement graphql client interface
    - add CreateUser graphql
    - add DeleteUser graphql
    - add FindAllUser graphql
    - add FindOneUser graphql
    - add FindUserByUsername graphql
    - add Generate2FA graphql
    - add Login graphql
    - add ResendActivationEmail graphql
    - add ResetPassword graphql
    - add RevokeTokens graphql
    - add graphql schema
    - add SendUpdateUsernameEmail graphql
    - add SendVerificationCodeEmail graphql
    - add UpdateUser graphql
    - add UpdateUserByUsername graphql
    - add ValidateEmailLink graphql
    - add Verify2FACode graphql
    
    Resolve CP-23

---
+++ composeApp/src/commonMain/kotlin/org/cdu/codefair/alertcity/view/MainScreen.kt
@@ -0,0 +1,36 @@
+package org.cdu.codefair.alertcity.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import org.cdu.codefair.alertcity.network.GraphQLClient
+
+@Composable
+fun MainScreen(username: String) {
+    val scope = rememberCoroutineScope()
+    val networkClient = remember { GraphQLClient() }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "Welcome, $username!")
+        LaunchedEffect(Unit) {
+            println("start fetch data")
+            try {
+//                val fetchData = networkClient.fetchData("https://192.168.1.103:51004/graphql")
+//                println(fetchData)
+            } catch (e: Exception) {
+                println(e.message)
+            }
+
+        }
+    }
+}
--- composeApp/src/iosMain/kotlin/org/cdu/codefair/alertcity/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/cdu/codefair/alertcity/MainViewController.kt
@@ -1,5 +1,6 @@
 package org.cdu.codefair.alertcity
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.cdu.codefair.alertcity.view.App
 
 fun MainViewController() = ComposeUIViewController { App() }
